80d04ce57c82bcb0e9f3ad6cc9c34a7f85d8a955
aanand-chouhan <44951031+aanand-chouhan@users.noreply.github.com>
2019-07-05 10:25:41 +0530

    Basic Authentication POC
    Basic Authentication in liferay 7 using REST API

---
+++ BasicAuthPOC/src/main/java/BasicAuthPOC/constants/BasicAuthPOCPortletKeys.java
@@ -0,0 +1,10 @@
+package BasicAuthPOC.constants;
+
+/**
+ * @author vidyayug
+ */
+public class BasicAuthPOCPortletKeys {
+
+	public static final String BasicAuthPOC = "basicauthpoc";
+
+}
---
+++ BasicAuthPOC/src/main/java/BasicAuthPOC/portlet/BasicAuthPOCPortlet.java
@@ -0,0 +1,28 @@
+package BasicAuthPOC.portlet;
+
+import BasicAuthPOC.constants.BasicAuthPOCPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author vidyayug
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + BasicAuthPOCPortletKeys.BasicAuthPOC,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class BasicAuthPOCPortlet extends MVCPortlet {
+}
